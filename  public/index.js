318380fd8cb63d5cc563731cdce7976f1a2526cf
Shaurya Aher <shauryaaher1200@gmail.com>
2021-09-13 17:29:11 +0530

    Add files

---
+++  public/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLtTnAVBuPzBL_dFK-ClbCHP2Dqp3gW4g",
+  authDomain: "stem-day-project-43876.firebaseapp.com",
+  projectId: "stem-day-project-43876",
+  storageBucket: "stem-day-project-43876.appspot.com",
+  messagingSenderId: "366923636539",
+  appId: "1:366923636539:web:f85b89b2ddc315e67ee11e"
+}
+
+initializeApp(firebaseConfig);
