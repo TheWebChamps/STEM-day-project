0957e2d236a9e1d00d274be5312239231c5f0746
Shaurya Aher <shauryaaher1200@gmail.com>
2021-09-18 18:30:06 +0530

    Add Authentication call onCreate() and onDelete().

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,25 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+admin.initializeApp();
+
+export const newSignUp = functions.auth.user().onCreate((user) => {
+    const data = {
+        uid: user.uid,
+        email: user.email
+    };
+    return admin.firestore().doc("userSignUps/{email}").set(data);
+});
+
+export const deleteAccount = functions.auth.user().onDelete((user) => {
+    const data = {
+        uid: user.uid,
+        email: user.email,
+        isDeleted: true
+    };
+    return admin.firestore().doc("userSignUps/{email}").set(data);
+});
